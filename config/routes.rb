ddae173c813ff88afb067bcef809883e9e5ac7b5
Chai Chai <c.chai@hotmail.co.uk>
2019-02-25 16:25:29 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  root to: 'cocktails#index'
+  resources :cocktails do
+    resources :doses
+    resources :ingredients
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doses, only: [:destroy]
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Dose.destroy
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+read_ingredients = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+
+ingredients = JSON.parse(read_ingredients)
+
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
